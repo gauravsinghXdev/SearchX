4de07452f4e1d37c4b2794bfc7d056548142410e
chandan adlak <chandanadlak4321@gmail.com>
2024-10-15 17:25:18 +0530

    merged with dev branch

--- app/documentCreation/page.jsx
+++ app/documentCreation/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
     <>
       <DocumentCreation>
         <div>
-          <DocumentCreationFirst />
+          <DocumentCreationFourth />
         </div>
       </DocumentCreation>
     </>
